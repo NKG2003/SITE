c97c66c703931fe9e5700e9cd62d0ff949025051
NKG <kaliollanurbek14@gmail.com>
2023-11-07 16:44:04 +0600

    2 commit

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
   <router-view />
 </template>
 
